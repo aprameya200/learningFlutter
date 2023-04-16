e4674f506c70dd8c9ec05faca16ae43f7a6ec62c
aprameyaneopane <aprameyaneopane@gmail.com>
2023-04-16 19:12:34 +0545

    usage of grid view

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,27 +1,8 @@
 class CatalogModel {
   //class to save and store containing list of items
-  final items = [
-    Item(
-        id: 1,
-        name: "Apple AirPods 3",
-        desc: "Airpods are the best for listening to music",
-        price: 200,
-        color: "White",
-        image:
-            "https://upload.wikimedia.org/wikipedia/commons/d/d2/AirPods_3rd_generation.jpg"),
-    Item(
-        id: 2,
-        name: "Samsung Galaxy S23",
-        desc: "Make everyday epic. Introducing Galaxy S22 and S22+ .",
-        price: 700,
-        color: "Green",
-        image:
-            "https://image-us.samsung.com/us/smartphones/galaxy-s22/images/gallery/R0-Green/FLRC-214-R0-Green-01-PDP-GALLERY-1600x1200.jpg")
-  ];
 
-  int getItemLength() {
-    return this.items.length;
-  }
+  //static variable and its value belongs to the class not any instance
+  static List<Item> items = [];
 }
 
 class Item {
@@ -40,4 +21,14 @@ class Item {
       required this.price,
       required this.color,
       required this.image});
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+        id: map["id"],
+        name: map["name"],
+        desc: map["desc"],
+        price: map["price"],
+        color: map["color"],
+        image: map["image"]);
+  }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:learningdart/models/catalog.dart';
 import 'package:learningdart/widgets/drawer.dart';
 
 import '../widgets/items_widget.dart';
 
-class HomePage extends StatelessWidget {
-  CatalogModel model = new CatalogModel(); //object of catalogue model
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //object of catalogue model
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+
+    final catalogJSON =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = json.decode(catalogJSON);
+
+    var productsArray = decodedData["products"];
+
+    //converts the productsArray into appropriate map type
+    //since one each element of productsArray is an item having has one string and value for different info
+    CatalogModel.items =
+        List.from(productsArray).map<Item>((e) => Item.fromMap(e)).toList();
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,18 +44,52 @@ class HomePage extends StatelessWidget {
         //has head , body and footer
         appBar: AppBar(
           title: Text(
-            "Home ",
+            "Catalog App ",
           ),
         ), //returns the build method in item widget
-        body: ListView.builder(
+        body: Padding(
           padding: EdgeInsets.all(16.0),
-          //loops through the given list
-          itemCount: model.getItemLength(),
-          itemBuilder: (BuildContext context, int index) {
-            //loops all available
-            return ItemsWidget(
-                model.items[index]); //returns the build method in item widget
-          },
+          child: (CatalogModel.items.length != null &&
+                  CatalogModel.items.isNotEmpty)
+              // ? ListView.builder(
+              //     //loops through the given list
+              //     itemCount: CatalogModel.items.length,
+              //     itemBuilder: (BuildContext context, int index) {
+              //       //loops all available
+              //       return ItemsWidget(CatalogModel.items[
+              //           index]); //returns the build method in item widget
+              //     },
+              //   )
+              ? GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2, //items per grid row
+                      mainAxisSpacing: 20,
+                      crossAxisSpacing: 20), //spacing
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                          header: Container(
+                            padding: EdgeInsets.all(12),
+                            color: Colors.amber,
+                            child: Text("${CatalogModel.items[index].name}"),
+                          ),
+                          footer: Container(
+                            padding: EdgeInsets.all(12),
+                            color: Colors.green,
+                            child: Text("\$${CatalogModel.items[index].price}"),
+                          ),
+                          //returns a tile in grid
+                          child:
+                              Image.network(CatalogModel.items[index].image)),
+                    );
+                  },
+                  itemCount: CatalogModel.items.length)
+              : Center(
+                  child: CircularProgressIndicator(),
+                ),
         ),
         drawer:
             MyDrawer()); //hamburger menu icon and feature also it is custom class made by user
